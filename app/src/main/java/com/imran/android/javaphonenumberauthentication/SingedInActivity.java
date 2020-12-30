a4c70cc87b77365d2e58a08acf2472d5180d205a
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2020-12-31 01:33:29 +0600

    signed in activity

---
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/SingedInActivity.java
@@ -0,0 +1,14 @@
+package com.imran.android.javaphonenumberauthentication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingedInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_singed_in);
+    }
+}
