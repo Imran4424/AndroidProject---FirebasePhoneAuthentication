b6c6df2ff454852a9bff2951a09accc512fbb097
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-03 02:17:42 +0600

    register

--- app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
@@ -2,12 +2,15 @@ package com.imran.android.javaphonenumberauthentication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegisterActivity extends AppCompatActivity {
     private Spinner spinnerCountry;
+    private EditText editTextCountryCode;
+    private EditText editTextNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
