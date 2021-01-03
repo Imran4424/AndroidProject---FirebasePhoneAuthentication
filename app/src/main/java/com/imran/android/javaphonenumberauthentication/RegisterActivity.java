4ee1917afaa7b359c17314a6a388bbb265226d93
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-04 00:24:44 +0600

    register activity

--- app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
@@ -3,7 +3,6 @@ package com.imran.android.javaphonenumberauthentication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
@@ -30,14 +29,14 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
         countryCodesList = Arrays.asList(getResources().getStringArray(R.array.country_code));
 
-        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
-                this,
-                R.array.country_names,
-                android.R.layout.simple_spinner_item
-        );
-
-        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinnerCountry.setAdapter(spinnerAdapter);
+//        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+//                this,
+//                R.array.country_names,
+//                android.R.layout.simple_spinner_item
+//        );
+//
+//        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+//        spinnerCountry.setAdapter(spinnerAdapter);
     }
 
     public void actionNext(View view) {
