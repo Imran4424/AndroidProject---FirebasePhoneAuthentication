a24d41508c44690e80e0f170b6e160a2a1406358
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-04 00:18:04 +0600

    register activity

--- app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
@@ -3,15 +3,19 @@ package com.imran.android.javaphonenumberauthentication;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.List;
+
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
     private Spinner spinnerCountry;
     private EditText editTextCountryCode;
     private EditText editTextNumber;
+    private List<String> countryCodesList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +23,17 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
         setContentView(R.layout.activity_register);
 
         spinnerCountry = findViewById(R.id.spinnerCountry);
+        editTextCountryCode = findViewById(R.id.editTextCountryCode);
+        editTextNumber = findViewById(R.id.editTextCode);
+
+        ArrayAdapter<CharSequence> spinnerAdapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.country_names,
+                android.R.layout.simple_spinner_item
+        );
+
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinnerCountry.setAdapter(spinnerAdapter);
     }
 
     public void actionNext(View view) {
