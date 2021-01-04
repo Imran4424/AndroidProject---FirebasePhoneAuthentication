e2aef93cb5709683fa617c7607863fbaf3cb4bb9
SHAH MD IMRAN HOSSAIN <akashganga.programming.534363@gmail.com>
2021-01-05 00:45:02 +0600

    register activity

--- app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/RegisterActivity.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RegisterActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private final String NUMBER_EXTRA = "NUMBER_EXTRA";
     private Spinner spinnerCountry;
     private EditText editTextCountryCode;
     private EditText editTextNumber;
@@ -61,8 +62,9 @@ public class RegisterActivity extends AppCompatActivity implements AdapterView.O
 
         String phoneNumber = editTextCountryCode.getText().toString() + editTextNumber.getText().toString();
 
-        Intent verfificationIntent = new Intent(this, VerificationActivity.class);
-        startActivity(verfificationIntent);
+        Intent verificationIntent = new Intent(this, VerificationActivity.class);
+        verificationIntent.putExtra(NUMBER_EXTRA, phoneNumber);
+        startActivity(verificationIntent);
     }
 
     @Override
--- app/src/main/java/com/imran/android/javaphonenumberauthentication/VerificationActivity.java
+++ app/src/main/java/com/imran/android/javaphonenumberauthentication/VerificationActivity.java
@@ -3,15 +3,29 @@ package com.imran.android.javaphonenumberauthentication;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class VerificationActivity extends AppCompatActivity {
+    private final String NUMBER_EXTRA = "NUMBER_EXTRA";
+    private TextView numberInVerification;
+    private EditText editTextCode;
+    private String numberFromIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_verification);
+
+        numberInVerification = findViewById(R.id.textNumberVerificaion);
+        editTextCode = findViewById(R.id.editTextCode);
+
+//        if (savedInstanceState == null) {
+//            numberFromIntent = getIntent().getStringExtra(NUMBER_EXTRA);
+//            numberInVerification.setText(numberFromIntent);
+//        }
     }
 
     public void actionWrongNumber(View view) {
